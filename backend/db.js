af016d1df8d9c3e7e90e3da72c0dffb944a67af2
Sangyog10 <116428979+Sangyog10@users.noreply.github.com>
2023-06-24 13:07:16 +0545

    server configured

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,37 @@
+// import mongoose from "mongoose";
+
+// const connectToMongo = () => {
+//   const uri =
+//     "mongodb+srv://fsu:fsu@12345@cluster0.0qrd73k.mongodb.net/?retryWrites=true&w=majority";
+
+//   mongoose
+//     .connect(uri, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//     })
+//     .then(() => {
+//       console.log("connected ");
+//     })
+//     .catch(() => {
+//       console.log("could not connect ");
+//     });
+// };
+// export default connectToMongo;
 import mongoose from "mongoose";
 
 const connectToMongo = () => {
   const url =
-    "mongodb+srv://somes:somes123@cluster0.bara8js.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://fsu:fsu123@cluster0.w4kxgv8.mongodb.net/?retryWrites=true&w=majority";
 
   mongoose
     .connect(url)
     .then(() => {
       console.log("connected ");
     })
-    .catch(() => {
-      console.log("could not connect hehe");
+    .catch((error) => {
+      console.log("could not connect hehe", error);
     });
 };
 export default connectToMongo;
